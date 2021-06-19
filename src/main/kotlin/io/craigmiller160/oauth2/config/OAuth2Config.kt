b42700f851bdf3fc9773b13ce81488e9b8d3403f
Craig Miller <craigmiller160@gmail.com>
2021-06-19 11:59:24 -0400

    Made OAuth2Config an abstract class with jwkset logic

--- src/main/kotlin/io/craigmiller160/oauth2/config/OAuth2Config.kt
+++ src/main/kotlin/io/craigmiller160/oauth2/config/OAuth2Config.kt
@@ -3,6 +3,7 @@ package io.craigmiller160.oauth2.config
 import com.nimbusds.jose.jwk.JWKSet
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import java.net.URL
 
 
 abstract class OAuth2Config {
@@ -26,4 +27,41 @@ abstract class OAuth2Config {
     private val log: Logger = LoggerFactory.getLogger(javaClass)
 
     lateinit var jwkSet: JWKSet
+
+    fun getOrDefaultCookiePath(): String {
+        if (cookiePath.isNotBlank()) {
+            return cookiePath
+        }
+        return "/"
+    }
+
+    fun getBaseWait(): Long {
+        return 1000
+    }
+
+    fun loadJWKSet(): JWKSet {
+        return JWKSet.load(URL("$authServerHost$jwkPath"))
+    }
+
+    fun getInsecurePathList(): List<String> {
+        return insecurePaths.split(",")
+                .map { it.trim() }
+                .filter { it.isNotBlank() }
+    }
+
+    fun tryToLoadJWKSet() {
+        log.info("Loading JWKSet")
+        for (i in 0 until 5) {
+            try {
+                jwkSet = loadJWKSet()
+                log.debug("Successfully loaded JWKSet")
+                return
+            } catch (ex: Exception) {
+                log.error("Error loading JWKSet", ex)
+                Thread.sleep(getBaseWait() * (i + 1))
+            }
+        }
+
+        throw RuntimeException("Failed to load JWKSet")
+    }
 }
