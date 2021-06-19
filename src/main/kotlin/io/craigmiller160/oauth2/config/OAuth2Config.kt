e36f66bc85bd32e9dbbeb643a4fed9eac261bd2f
Craig Miller <craigmiller160@gmail.com>
2021-06-19 11:58:46 -0400

    Moving dependencies and jwkset code to lib

--- src/main/kotlin/io/craigmiller160/oauth2/config/OAuth2Config.kt
+++ src/main/kotlin/io/craigmiller160/oauth2/config/OAuth2Config.kt
@@ -1,17 +1,29 @@
 package io.craigmiller160.oauth2.config
 
-// TODO move documentation about all properties to this. all property keys should be consistent
-interface OAuth2Config {
-    var authServerHost: String
-    var authCodeRedirectUri: String
-    var clientName: String
-    var clientKey: String
-    var clientSecret: String
-    var cookieName: String
-    var postAuthRedirect: String
-    var cookieMaxAgeSecs: Long
-    var cookiePath: String
-    var authLoginBaseUri: String
-    var insecurePaths: String
-    var authCodeWaitMins: Long
+import com.nimbusds.jose.jwk.JWKSet
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+
+abstract class OAuth2Config {
+    abstract var authServerHost: String
+    abstract var authCodeRedirectUri: String
+    abstract var clientName: String
+    abstract var clientKey: String
+    abstract var clientSecret: String
+    abstract var cookieName: String
+    abstract var postAuthRedirect: String
+    abstract var cookieMaxAgeSecs: Long
+    abstract var cookiePath: String
+    abstract var authLoginBaseUri: String
+    abstract var insecurePaths: String
+    abstract var authCodeWaitMins: Long
+
+    val jwkPath = "/jwk"
+    val tokenPath = "/oauth/token"
+    val authCodeLoginPath = "/ui/login"
+
+    private val log: Logger = LoggerFactory.getLogger(javaClass)
+
+    lateinit var jwkSet: JWKSet
 }
