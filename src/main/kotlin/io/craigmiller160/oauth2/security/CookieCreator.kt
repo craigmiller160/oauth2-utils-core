96de7ee6d1585becb4193f999bbcaef05727eb0b
Craig Miller <craigmiller160@gmail.com>
2021-07-16 16:52:23 -0400

    Began implementing service

---
+++ src/main/kotlin/io/craigmiller160/oauth2/security/CookieCreator.kt
@@ -0,0 +1,21 @@
+package io.craigmiller160.oauth2.security
+
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+
+object CookieCreator {
+
+    private const val DEFAULT_MAX_AGE = 24 * 60 * 60
+
+    val cookieExpFormat: DateTimeFormatter = DateTimeFormatter.ofPattern("EEE, dd MMM yyyy HH:mm:ss z")
+
+    fun createTokenCookie(cookieName: String, token: String, maxAgeSecs: Int = DEFAULT_MAX_AGE): String {
+        val expires = ZonedDateTime.now(ZoneId.of("GMT"))
+                .plusHours(24)
+        val expiresString = cookieExpFormat.format(expires)
+
+        return "$cookieName=$token; Max-Age=$maxAgeSecs; Expires=$expiresString; Secure; HttpOnly; SameSite=strict; Path=/funcoast-hi"
+    }
+
+}
--- src/main/kotlin/io/craigmiller160/oauth2/service/AbstractOAuth2Service.kt
+++ src/main/kotlin/io/craigmiller160/oauth2/service/AbstractOAuth2Service.kt
@@ -1,9 +1,27 @@
 package io.craigmiller160.oauth2.service
 
-abstract class AbstractOAuth2Service : OAuth2Service {
+import io.craigmiller160.oauth2.config.OAuth2Config
+import io.craigmiller160.oauth2.domain.repository.AppRefreshTokenRepository
+import io.craigmiller160.oauth2.dto.AuthUserDto
+import io.craigmiller160.oauth2.security.AuthenticatedUser
+import io.craigmiller160.oauth2.security.CookieCreator
 
-    protected abstract fun getAuthUserContext()
+abstract class AbstractOAuth2Service(
+        private val oAuth2Config: OAuth2Config,
+        private val appRefreshTokenRepo: AppRefreshTokenRepository
+) : OAuth2Service {
 
-    // TODO implement interface methods
+    protected abstract fun getAuthUserContext(): AuthenticatedUser
+
+    override fun logout(): String {
+        val authUser = getAuthUserContext()
+        appRefreshTokenRepo.removeByTokenId(authUser.tokenId)
+        return CookieCreator.createTokenCookie(oAuth2Config.cookieName, oAuth2Config.getOrDefaultCookiePath(), "", 0)
+    }
+
+    override fun getAuthenticatedUser(): AuthUserDto {
+        val authUser = getAuthUserContext()
+        TODO("Finish this")
+    }
 
 }
