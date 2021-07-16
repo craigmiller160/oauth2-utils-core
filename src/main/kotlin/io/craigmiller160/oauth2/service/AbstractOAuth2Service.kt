7a96d9b15bb2e0fabe9453fce6d1dd4981cff8d2
Craig Miller <craigmiller160@gmail.com>
2021-07-16 16:40:18 -0400

    Began migrating code

---
+++ src/main/kotlin/io/craigmiller160/oauth2/service/AbstractOAuth2Service.kt
@@ -0,0 +1,9 @@
+package io.craigmiller160.oauth2.service
+
+abstract class AbstractOAuth2Service : OAuth2Service {
+
+    protected abstract fun getAuthUserContext()
+
+    // TODO implement interface methods
+
+}
