200f4b18203cb109e1e30011bcc755ca7295aec3
Craig Miller <craigmiller160@gmail.com>
2021-07-16 14:14:57 -0400

    Removed unnecessary generics

--- src/main/kotlin/io/craigmiller160/oauth2/domain/repository/AppRefreshTokenRepository.kt
+++ src/main/kotlin/io/craigmiller160/oauth2/domain/repository/AppRefreshTokenRepository.kt
@@ -2,7 +2,7 @@ package io.craigmiller160.oauth2.domain.repository
 
 import io.craigmiller160.oauth2.domain.entity.AppRefreshToken
 
-interface AppRefreshTokenRepository<T : AppRefreshToken> {
-    fun findByTokenId(tokenId: String): T
+interface AppRefreshTokenRepository {
+    fun findByTokenId(tokenId: String): AppRefreshToken?
     fun removeByTokenId(tokenId: String): Int
 }
