f48bbd0bd406c740785460a5220f17d603137159
Craig Miller <craigmiller160@gmail.com>
2021-07-16 16:28:34 -0400

    Fixed a compatibility error

--- src/main/kotlin/io/craigmiller160/oauth2/domain/repository/AppRefreshTokenRepository.kt
+++ src/main/kotlin/io/craigmiller160/oauth2/domain/repository/AppRefreshTokenRepository.kt
@@ -5,6 +5,6 @@ import io.craigmiller160.oauth2.domain.entity.AppRefreshToken
 interface AppRefreshTokenRepository {
     fun findByTokenId(tokenId: String): AppRefreshToken?
     fun removeByTokenId(tokenId: String)
-    fun deleteById(id: Long)
+    fun deleteById(id: java.lang.Long)
     fun save(token: AppRefreshToken): AppRefreshToken
 }
