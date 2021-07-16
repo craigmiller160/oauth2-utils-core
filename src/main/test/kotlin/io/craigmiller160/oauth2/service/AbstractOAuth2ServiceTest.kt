bdd2d98b845b77ca15163db8ecf859302b46c879
Craig Miller <craigmiller160@gmail.com>
2021-07-16 16:47:57 -0400

    Stubbed out tests

---
+++ src/main/test/kotlin/io/craigmiller160/oauth2/service/AbstractOAuth2ServiceTest.kt
@@ -0,0 +1,17 @@
+package io.craigmiller160.oauth2.service
+
+import org.junit.jupiter.api.Test
+
+class AbstractOAuth2ServiceTest {
+
+    @Test
+    fun `logout()`() {
+        TODO("Finish this")
+    }
+
+    @Test
+    fun `getAuthenticatedUser()`() {
+        TODO("Finish this")
+    }
+
+}
