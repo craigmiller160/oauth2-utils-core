bcc10afd862c5176b963b7f280b87e2a80717120
Craig Miller <craigmiller160@gmail.com>
2021-07-16 11:17:07 -0400

    Code cleanup

--- src/main/kotlin/io/craigmiller160/oauth2/client/AuthServerClientRequest.kt
+++
@@ -1,9 +0,0 @@
-package io.craigmiller160.oauth2.client
-
-// TODO might not need this
-data class AuthServerClientRequest(
-        val url: String,
-        val clientKey: String,
-        val clientSecret: String,
-        val body: Map<String,String>
-)
--- src/main/kotlin/io/craigmiller160/oauth2/exception/InvalidResponseBodyException.kt
+++
@@ -1,6 +0,0 @@
-package io.craigmiller160.oauth2.exception
-
-import java.lang.RuntimeException
-
-// TODO may not need this
-class InvalidResponseBodyException : RuntimeException()
