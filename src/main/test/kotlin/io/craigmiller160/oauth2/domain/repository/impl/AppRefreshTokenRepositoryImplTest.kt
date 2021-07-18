d3c48a9f99ca3915a4f4240771f38c65e6598d53
Craig Miller <craigmiller160@gmail.com>
2021-07-18 14:00:40 -0400

    Got all tests working

--- src/main/test/kotlin/io/craigmiller160/oauth2/domain/repository/impl/AppRefreshTokenRepositoryImplTest.kt
+++ src/main/test/kotlin/io/craigmiller160/oauth2/domain/repository/impl/AppRefreshTokenRepositoryImplTest.kt
@@ -1,11 +1,14 @@
 package io.craigmiller160.oauth2.domain.repository.impl
 
 import io.craigmiller160.oauth2.domain.SqlConnectionProvider
-import io.craigmiller160.oauth2.domain.repository.AppRefreshTokenRepository
+import io.craigmiller160.oauth2.domain.entity.AppRefreshToken
 import org.h2.tools.Server
 import org.junit.jupiter.api.*
 import java.nio.file.Files
 import java.sql.DriverManager
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+import kotlin.test.assertTrue
 
 class AppRefreshTokenRepositoryImplTest {
 
@@ -56,27 +59,133 @@ class AppRefreshTokenRepositoryImplTest {
 
     @Test
     fun `deleteById()`() {
-        TODO("Finish this")
+        val id: Long = DriverManager.getConnection(getJdbcUrl()).use { conn ->
+            conn.prepareStatement("INSERT INTO app_refresh_tokens (token_id, refresh_token) VALUES(?,?)").use { stmt ->
+                stmt.setString(1, TOKEN_ID)
+                stmt.setString(2, REFRESH_TOKEN)
+                stmt.executeUpdate()
+            }
+            conn.createStatement().use { stmt -> stmt.executeQuery("SELECT * FROM app_refresh_tokens").use { rs ->
+                rs.next()
+                rs.getLong("id")
+            } }
+        }
+
+        repo.deleteById(id)
+
+        DriverManager.getConnection(getJdbcUrl()).use { conn ->
+            conn.createStatement().use { stmt ->
+                stmt.executeQuery("SELECT COUNT(*) FROM app_refresh_tokens").use { rs ->
+                    assertTrue { rs.next() }
+                    assertEquals(0, rs.getLong(1))
+                }
+            }
+        }
     }
 
     @Test
     fun `removeByTokenId()`() {
-        TODO("Finish this")
+        DriverManager.getConnection(getJdbcUrl()).use { conn ->
+            conn.prepareStatement("INSERT INTO app_refresh_tokens (token_id, refresh_token) VALUES(?,?)").use { stmt ->
+                stmt.setString(1, TOKEN_ID)
+                stmt.setString(2, REFRESH_TOKEN)
+                stmt.executeUpdate()
+            }
+        }
+
+        repo.removeByTokenId(TOKEN_ID)
+
+        DriverManager.getConnection(getJdbcUrl()).use { conn ->
+            conn.createStatement().use { stmt ->
+                stmt.executeQuery("SELECT COUNT(*) FROM app_refresh_tokens").use { rs ->
+                    assertTrue { rs.next() }
+                    assertEquals(0, rs.getLong(1))
+                }
+            }
+        }
     }
 
     @Test
     fun `findByTokenId()`() {
-        TODO("Finish this")
+        DriverManager.getConnection(getJdbcUrl()).use { conn ->
+            conn.prepareStatement("INSERT INTO app_refresh_tokens (token_id, refresh_token) VALUES(?,?)").use { stmt ->
+                stmt.setString(1, TOKEN_ID)
+                stmt.setString(2, REFRESH_TOKEN)
+                stmt.executeUpdate()
+            }
+        }
+
+        val result = repo.findByTokenId(TOKEN_ID)
+        assertNotNull(result)
+        assertTrue { result.id > 0 }
+        assertEquals(TOKEN_ID, result.tokenId)
+        assertEquals(REFRESH_TOKEN, result.refreshToken)
     }
 
     @Test
     fun `save() new token`() {
-        TODO("Finish this")
+        val token = AppRefreshToken(
+                id = 0,
+                tokenId = TOKEN_ID,
+                refreshToken = REFRESH_TOKEN
+        )
+        val result = repo.save(token)
+        assertEquals(token.copy(id = result.id), result)
+
+        DriverManager.getConnection(getJdbcUrl()).use { conn ->
+            conn.createStatement().use { stmt ->
+                stmt.executeQuery("SELECT COUNT(*) FROM app_refresh_tokens").use { rs ->
+                    assertTrue { rs.next() }
+                    assertEquals(1, rs.getLong(1))
+                }
+
+                stmt.executeQuery("SELECT * FROM app_refresh_tokens").use { rs ->
+                    assertTrue { rs.next() }
+                    assertEquals(result.id, rs.getLong("id"))
+                    assertEquals(result.tokenId, rs.getString("token_id"))
+                    assertEquals(result.refreshToken, rs.getString("refresh_token"))
+                }
+            }
+        }
     }
 
     @Test
     fun `save() existing token`() {
-        TODO("Finish this")
+        val id: Long = DriverManager.getConnection(getJdbcUrl()).use { conn ->
+            conn.prepareStatement("INSERT INTO app_refresh_tokens (token_id, refresh_token) VALUES(?,?)").use { stmt ->
+                stmt.setString(1, TOKEN_ID)
+                stmt.setString(2, REFRESH_TOKEN)
+                stmt.executeUpdate()
+            }
+            conn.createStatement().use { stmt -> stmt.executeQuery("SELECT * FROM app_refresh_tokens").use { rs ->
+                rs.next()
+                rs.getLong("id")
+            } }
+        }
+
+        val token = AppRefreshToken(
+                id = id,
+                tokenId = "${TOKEN_ID}_2",
+                refreshToken = "${REFRESH_TOKEN}_2"
+        )
+        val result = repo.save(token)
+        assertEquals(token, result)
+
+        DriverManager.getConnection(getJdbcUrl()).use { conn ->
+            conn.createStatement().use { stmt ->
+                stmt.executeQuery("SELECT COUNT(*) FROM app_refresh_tokens").use { rs ->
+                    assertTrue { rs.next() }
+                    assertEquals(1, rs.getLong(1))
+                }
+
+                stmt.executeQuery("SELECT * FROM app_refresh_tokens").use { rs ->
+                    assertTrue { rs.next() }
+                    assertEquals(result.id, rs.getLong("id"))
+                    assertEquals(result.tokenId, rs.getString("token_id"))
+                    assertEquals(result.refreshToken, rs.getString("refresh_token"))
+                }
+            }
+        }
     }
 
 }
