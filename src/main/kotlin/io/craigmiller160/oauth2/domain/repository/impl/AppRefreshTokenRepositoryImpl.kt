2c62fcdd0e74d8b478bf2631629e0c51c91771b2
Craig Miller <craigmiller160@gmail.com>
2021-07-18 13:58:01 -0400

    Implemented all repo methods

--- src/main/kotlin/io/craigmiller160/oauth2/domain/repository/impl/AppRefreshTokenRepositoryImpl.kt
+++ src/main/kotlin/io/craigmiller160/oauth2/domain/repository/impl/AppRefreshTokenRepositoryImpl.kt
@@ -1,22 +1,112 @@
 package io.craigmiller160.oauth2.domain.repository.impl
 
+import io.craigmiller160.oauth2.domain.SqlConnectionProvider
 import io.craigmiller160.oauth2.domain.entity.AppRefreshToken
 import io.craigmiller160.oauth2.domain.repository.AppRefreshTokenRepository
+import java.sql.Connection
+import java.sql.ResultSet
 
-class AppRefreshTokenRepositoryImpl : AppRefreshTokenRepository {
+// TODO need to setup inject in jax-rs
+class AppRefreshTokenRepositoryImpl (
+        private val sqlConnectionProvider: SqlConnectionProvider
+) : AppRefreshTokenRepository {
+    private val deleteById = """
+        DELETE FROM app_refresh_tokens
+        WHERE id = ?
+    """.trimIndent()
+    private val selectByTokenId = """
+        SELECT *
+        FROM app_refresh_tokens
+        WHERE token_id = ?
+    """.trimIndent()
+    private val deleteByTokenId = """
+        DELETE FROM app_refresh_tokens
+        WHERE token_id = ?
+    """.trimIndent()
+    private val insertRefreshToken = """
+        INSERT INTO app_refresh_tokens (token_id, refresh_token)
+        VALUES (?,?)
+    """.trimIndent()
+    private val updateRefreshToken = """
+        UPDATE app_refresh_tokens 
+        SET token_id = ?, refresh_token = ?
+        WHERE id = ?
+    """.trimIndent()
+
+    private fun resultSetToEntity(rs: ResultSet): AppRefreshToken =
+            AppRefreshToken(
+                    id = rs.getLong("id"),
+                    tokenId = rs.getString("token_id"),
+                    refreshToken = rs.getString("refresh_token")
+            )
+
+    private fun doFindByTokenId(conn: Connection, tokenId: String): AppRefreshToken? {
+        return conn.prepareStatement(selectByTokenId).use { stmt ->
+            stmt.setString(1, tokenId)
+            stmt.executeQuery().use { rs ->
+                if (rs.next()) {
+                    resultSetToEntity(rs)
+                } else {
+                    null
+                }
+            }
+        }
+    }
+
     override fun findByTokenId(tokenId: String): AppRefreshToken? {
-        TODO("Not yet implemented")
+        return sqlConnectionProvider.provide().use { conn ->
+            doFindByTokenId(conn, tokenId)
+        }
     }
 
     override fun removeByTokenId(tokenId: String) {
-        TODO("Not yet implemented")
+        sqlConnectionProvider.provide().use { conn ->
+            conn.prepareStatement(deleteByTokenId).use { stmt ->
+                stmt.setString(1, tokenId)
+                stmt.executeUpdate()
+            }
+            conn.commit()
+        }
     }
 
     override fun deleteById(id: Long) {
-        TODO("Not yet implemented")
+        sqlConnectionProvider.provide().use { conn ->
+            conn.prepareStatement(deleteById).use { stmt ->
+                stmt.setLong(1, id)
+                stmt.executeUpdate()
+            }
+            conn.commit()
+        }
+    }
+
+    private fun insertToken(conn: Connection, token: AppRefreshToken) {
+        conn.prepareStatement(insertRefreshToken).use { stmt ->
+            stmt.setString(1, token.tokenId)
+            stmt.setString(2, token.refreshToken)
+            stmt.executeUpdate()
+        }
+        conn.commit()
+    }
+
+    private fun updateToken(conn: Connection, token: AppRefreshToken) {
+        conn.prepareStatement(updateRefreshToken).use { stmt ->
+            stmt.setString(1, token.tokenId)
+            stmt.setString(2, token.refreshToken)
+            stmt.setLong(3, token.id)
+            stmt.executeUpdate()
+        }
+        conn.commit()
     }
 
     override fun save(token: AppRefreshToken): AppRefreshToken {
-        TODO("Not yet implemented")
+        return sqlConnectionProvider.provide().use { conn ->
+            if (token.id > 0) {
+                updateToken(conn, token)
+            } else {
+                insertToken(conn, token)
+            }
+
+            doFindByTokenId(conn, token.tokenId)!!
+        }
     }
 }
