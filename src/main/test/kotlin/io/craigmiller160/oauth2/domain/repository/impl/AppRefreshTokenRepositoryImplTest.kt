e40c8f0130ef64a67a29d65df76130425eded0dc
Craig Miller <craigmiller160@gmail.com>
2021-07-18 13:58:56 -0400

    Added test setup logic

--- src/main/test/kotlin/io/craigmiller160/oauth2/domain/repository/impl/AppRefreshTokenRepositoryImplTest.kt
+++ src/main/test/kotlin/io/craigmiller160/oauth2/domain/repository/impl/AppRefreshTokenRepositoryImplTest.kt
@@ -1,8 +1,58 @@
 package io.craigmiller160.oauth2.domain.repository.impl
 
-import org.junit.jupiter.api.Test
+import io.craigmiller160.oauth2.domain.SqlConnectionProvider
+import io.craigmiller160.oauth2.domain.repository.AppRefreshTokenRepository
+import org.h2.tools.Server
+import org.junit.jupiter.api.*
+import java.nio.file.Files
+import java.sql.DriverManager
 
 class AppRefreshTokenRepositoryImplTest {
+
+    companion object {
+        private const val TOKEN_ID = "tokenId"
+        private const val REFRESH_TOKEN = "refreshToken"
+
+        lateinit var server: Server
+
+        @BeforeAll
+        @JvmStatic
+        fun createServer() {
+            server = Server.createTcpServer("-ifNotExists")
+            server.start()
+        }
+
+        @AfterAll
+        @JvmStatic
+        fun destroyServer() {
+            server.stop()
+        }
+    }
+
+    private val tempFile = Files.createTempFile("prefix", "suffix")
+    private lateinit var repo: AppRefreshTokenRepositoryImpl
+
+    private fun getJdbcUrl(): String {
+        val schema = javaClass.classLoader.getResource("schema.sql").toURI().path
+        return "jdbc:h2:${server.url}/${tempFile.toFile().absolutePath};MODE=PostgreSQL;INIT=RUNSCRIPT FROM '$schema'"
+    }
+
+    @BeforeEach
+    fun setup() {
+        val connProvider = SqlConnectionProvider {
+            DriverManager.getConnection(getJdbcUrl())
+        }
+        repo = AppRefreshTokenRepositoryImpl(connProvider)
+    }
+
+    @AfterEach
+    fun clean() {
+        DriverManager.getConnection(getJdbcUrl()).use { conn ->
+            conn.createStatement().use { stmt ->
+                stmt.executeUpdate("DELETE FROM app_refresh_tokens")
+            }
+        }
+    }
 
     @Test
     fun `deleteById()`() {
