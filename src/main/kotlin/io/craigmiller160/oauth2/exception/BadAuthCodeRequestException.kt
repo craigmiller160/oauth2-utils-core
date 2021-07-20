e067ff4400f1f229787ce1450e618d73f1a0f4ef
Craig Miller <craigmiller160@gmail.com>
2021-07-20 16:06:49 -0400

    Cleaned up TODO

--- src/main/kotlin/io/craigmiller160/oauth2/exception/BadAuthCodeRequestException.kt
+++ src/main/kotlin/io/craigmiller160/oauth2/exception/BadAuthCodeRequestException.kt
@@ -2,5 +2,4 @@ package io.craigmiller160.oauth2.exception
 
 import java.lang.RuntimeException
 
-// TODO add to Spring exception handler
 class BadAuthCodeRequestException(msg: String) : RuntimeException(msg)
