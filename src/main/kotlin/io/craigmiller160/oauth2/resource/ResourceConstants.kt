92782bef6d65128d05f129048e8dd3b839fc06c2
Craig Miller <craigmiller160@gmail.com>
2021-07-20 16:12:33 -0400

    Added constants for resource paths

---
+++ src/main/kotlin/io/craigmiller160/oauth2/resource/ResourceConstants.kt
@@ -0,0 +1,9 @@
+package io.craigmiller160.oauth2.resource
+
+object ResourceConstants {
+    const val ROOT_PATH = "/oauth"
+    const val AUTHCODE_LOGIN_PATH = "/authcode/login"
+    const val AUTHCODE_CODE_PATH = "/authcode/code"
+    const val LOGOUT_PATH = "/logout"
+    const val AUTH_USER_PATH = "/user"
+}
