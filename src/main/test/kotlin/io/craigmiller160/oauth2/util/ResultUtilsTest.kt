a4b5410ed5eabb3718a5ed3d6bfcda2e91cf4a7d
Craig Miller <craigmiller160@gmail.com>
2021-07-25 16:25:59 -0400

    Stubbed out tests

---
+++ src/main/test/kotlin/io/craigmiller160/oauth2/util/ResultUtilsTest.kt
@@ -0,0 +1,22 @@
+package io.craigmiller160.oauth2.util
+
+import org.junit.jupiter.api.Test
+
+class ResultUtilsTest {
+
+    @Test
+    fun `chain a successful transform onto a successful Result`() {
+        TODO("Finish this")
+    }
+
+    @Test
+    fun `chain a failed transform onto a successful Result`() {
+        TODO("Finish this")
+    }
+
+    @Test
+    fun `chain a successful transform onto a failed Result`() {
+        TODO("Finish this")
+    }
+
+}
