f26af93c314fa20a73bca1e74068a779c9278c57
Craig Miller <craigmiller160@gmail.com>
2021-07-25 16:31:08 -0400

    Finished all tests

--- src/main/test/kotlin/io/craigmiller160/oauth2/util/ResultUtilsTest.kt
+++ src/main/test/kotlin/io/craigmiller160/oauth2/util/ResultUtilsTest.kt
@@ -1,6 +1,7 @@
 package io.craigmiller160.oauth2.util
 
 import org.junit.jupiter.api.Test
+import java.io.IOException
 import java.lang.RuntimeException
 import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
@@ -19,23 +20,35 @@ class ResultUtilsTest {
 
     @Test
     fun `chain a failed transform onto a successful Result`() {
-        assertFailsWith<RuntimeException> {
+        assertFailsWith<IOException> {
             runCatching {
                 "Hello World"
             }
-                    .chain { Result.failure(RuntimeException("Dying")) }
+                    .chain { Result.failure<Int>(IOException("IO Dying")) }
                     .getOrThrow()
         }
     }
 
     @Test
     fun `chain a successful transform onto a failed Result`() {
-        TODO("Finish this")
+        assertFailsWith<RuntimeException> {
+            runCatching {
+                throw RuntimeException("Dying")
+            }
+                    .chain { Result.success(123) }
+                    .getOrThrow()
+        }
     }
 
     @Test
     fun `chain a failed transform onto a failed Result`() {
-        TODO("Finish this")
+        assertFailsWith<RuntimeException> {
+            runCatching {
+                throw RuntimeException("Dying")
+            }
+                    .chain { Result.failure<Int>(IOException("IO Dying")) }
+                    .getOrThrow()
+        }
     }
 
 }
