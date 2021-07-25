d720be0c3dfd65bf6725706fb7c83a9b571c46a6
Craig Miller <craigmiller160@gmail.com>
2021-07-25 16:24:31 -0400

    Working on Result chain method

--- src/main/kotlin/io/craigmiller160/oauth2/util/ResultUtils.kt
+++ src/main/kotlin/io/craigmiller160/oauth2/util/ResultUtils.kt
@@ -2,3 +2,9 @@ package io.craigmiller160.oauth2.util
 
 // TODO move this into its own library if it works
 
+fun <T,R> Result<T>.chain(transform: (T) -> Result<R>): Result<R> {
+    if (isSuccess) {
+        return transform(getOrThrow())
+    }
+    return this as Result<R>
+}
