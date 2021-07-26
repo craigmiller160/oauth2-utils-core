6eabef4c80bbd734d11e81141f1b69beaf09176f
Craig Miller <craigmiller160@gmail.com>
2021-07-26 17:17:06 -0400

    Added status codes to exception

--- src/main/kotlin/io/craigmiller160/oauth2/exception/OAuth2Exception.kt
+++ src/main/kotlin/io/craigmiller160/oauth2/exception/OAuth2Exception.kt
@@ -2,4 +2,6 @@ package io.craigmiller160.oauth2.exception
 
 import java.lang.RuntimeException
 
-open class OAuth2Exception(msg: String, cause: Throwable? = null) : RuntimeException(msg, cause)
+abstract class OAuth2Exception(msg: String, cause: Throwable? = null) : RuntimeException(msg, cause) {
+    abstract val statusCode: Int
+}
