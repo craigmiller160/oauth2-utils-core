75bd322ee475701abe03220222f9d6cfc75b68a1
Craig Miller <craigmiller160@gmail.com>
2022-06-20 11:20:05 -0400

    Feature/user (#16)
    * Added user ID to auth user
    
    * Updated auth user DTO
    
    * Updated dependency versions

--- src/main/kotlin/io/craigmiller160/oauth2/security/AuthenticatedUser.kt
+++ src/main/kotlin/io/craigmiller160/oauth2/security/AuthenticatedUser.kt
@@ -1,6 +1,7 @@
 package io.craigmiller160.oauth2.security
 
 interface AuthenticatedUser {
+    val userId: Long
     val userName: String
     val roles: List<String>
     val firstName: String
--- src/test/kotlin/io/craigmiller160/oauth2/service/impl/OAuth2ServiceImplTest.kt
+++ src/test/kotlin/io/craigmiller160/oauth2/service/impl/OAuth2ServiceImplTest.kt
@@ -16,6 +16,7 @@ import kotlin.test.assertEquals
 class OAuth2ServiceImplTest {
 
     companion object {
+        const val USER_ID = 1L
         const val USER_NAME = "UserName"
         val ROLES = listOf("Role")
         const val FIRST_NAME = "firstName"
@@ -24,6 +25,7 @@ class OAuth2ServiceImplTest {
         const val COOKIE_NAME = "cookieName"
         const val COOKIE_PATH = "cookiePath"
         private val authUser = AuthUserImpl(
+                USER_ID,
                 USER_NAME,
                 ROLES,
                 FIRST_NAME,
@@ -69,9 +71,11 @@ class OAuth2ServiceImplTest {
         assertEquals(authUser.roles, result.roles)
         assertEquals(authUser.firstName, result.firstName)
         assertEquals(authUser.lastName, result.lastName)
+        assertEquals(authUser.userId, result.userId)
     }
 
     data class AuthUserImpl(
+            override val userId: Long,
             override val userName: String,
             override val roles: List<String>,
             override val firstName: String,
